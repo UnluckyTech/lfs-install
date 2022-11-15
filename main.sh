3f511f5bca81ad824f1d694ade661418e06d1008
UnluckyTech <stawse99@protonmail.com>
2022-11-15 15:02:58 -0500

    Update main.sh
    Simple menu for now. Getting into the partitioning now.

--- main.sh
+++ main.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-echo 'This is a first bash script that is waiting for a response'
-. prereq.sh
-sleep 3
-echo 'So what happened?'
+while true
+do
+
+    echo '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+    echo '%%%%%%%%% LFS  Installer %%%%%%%%'
+    echo '%%%% Choose how you want to  %%%%'
+    echo '%%%%%%%%%%%% begin %%%%%%%%%%%%%%'
+    echo '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+    echo ''
+    echo '1. Check Prerequisites'
+    echo '2. Partition'
+
+    read option 
+
+    if [[ $option == "1" ]]; then 
+        . prereq.sh
+    elif [[ $option == "2" ]]; then
+    fdisk -l
+    else
+        2>/dev/null
+        echo 'Incorrect command. Try again.'
+    fi
+done

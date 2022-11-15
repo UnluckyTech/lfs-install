94822aaadf7126032765b5cb3ece5513cde3a4e6
UnluckyTech <stawse99@protonmail.com>
2022-11-15 15:19:19 -0500

    Update main.sh
    Preconfigure environment

--- main.sh
+++ main.sh
@@ -8,14 +8,22 @@ do
     echo '%%%%%%%%%%%% begin %%%%%%%%%%%%%%'
     echo '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
     echo ''
-    echo '1. Check Prerequisites'
-    echo '2. Partition'
+    echo '1. Configure VM'
+    echo '2. Check Prerequisites'
+    echo '3. Partition Drive'
 
     read option 
 
-    if [[ $option == "1" ]]; then 
+    if [[ $option == "1" ]]; then
+        sudo pacman -S rxvt-unicode
+        sudo systemctl start sshd
+        if [ "$EUID" -ne 0 ]
+            then echo "Please run as root"
+            exit
+        fi
+    elif [[ $option == "2" ]]; then 
         . prereq.sh
-    elif [[ $option == "2" ]]; then
+    elif [[ $option == "3" ]]; then
     fdisk -l
     else
         2>/dev/null

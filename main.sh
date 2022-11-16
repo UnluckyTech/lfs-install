1d5e7c92a6495d68935925c105b8c345d6b22683
UnluckyTech <stawse99@protonmail.com>
2022-11-15 20:31:24 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -48,7 +48,7 @@ do
         mkfs.ext4 "$device"
         echo "Will now partition the drive"
         ( echo 'n' ; echo 'p' ; echo '1' ; echo '2048' ; echo '+1G' ; echo 't' ; echo '82' ; echo 'w' ) | fdisk "$device"
-        ( echo 'p' ; echo '2' ; echo ' ' ; echo ' ' ; echo 'w' ) | fdisk "$device"
+        ( echo 'n' ; echo 'p' ; echo '2' ; echo ' ' ; echo ' ' ; echo 'y' ; echo 'w' ) | fdisk "$device"
         fdisk -l
         echo "for drive $device you should see 2 partitions "
         sleep 3

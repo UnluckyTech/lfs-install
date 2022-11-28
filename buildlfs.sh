16b161d648206b3f33e092f5dad6b520bb4cfa90
UnluckyTech <stawse99@protonmail.com>
2022-11-28 13:10:55 -0500

    Temp

---
+++ buildlfs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+while true
+do
+    echo ''
+    echo '*********************************'
+    echo '********** LFS  Builder *********'
+    echo '*********************************'
+    echo '1. IDK'
+    echo '2. IDK'
+    echo '3. IDK'
+    echo '4. IDK'
+    echo '5. Return to Installer'
+    read option
+    if [[ $option == "1" ]]; then
+        echo 'nuh'
+    elif [[ $option == "2" ]]; then
+        echo 'nuh'
+    elif [[ $option == "3" ]]; then 
+        echo 'nuh'
+    elif [[ $option == "4" ]]; then
+        echo 'nuh'
+    elif [[ $option == "5" ]]; then
+        . main.sh
+    else
+        2>/dev/null
+        echo 'Incorrect command. Try again.'
+    fi
+        
+done

b84ff035cf4f85076208d6a3d62d420b41603c79
UnluckyTech <stawse99@protonmail.com>
2023-02-05 12:08:00 -0500

    Update temptools.sh

--- temptools.sh
+++ temptools.sh
@@ -6,14 +6,24 @@ do
     echo '*********************************'
     echo '********* LFS  TempTools ********'
     echo '*********************************'
-    echo '1. IDK'
+    echo '1. Packages and Patches'
     echo '2. IDK'
     echo '3. IDK'
     echo '4. IDK'
     echo '5. Return to Installer'
     read option
     if [[ $option == "1" ]]; then
-        echo 'nuh'
+        echo "Creating Directory and Setting Permission's"
+        sleep 1
+        mkdir -v $LFS/sources
+        chmod -v a+wt $LFS/sources
+        echo "Fetching Required Packages"
+        sleep 1
+        wget -P $LFS/sources https://www.linuxfromscratch.org/lfs/view/stable/wget-list-sysv
+        wget --input-file=wget-list-sysv --continue --directory-prefix=$LFS/sources
+        echo "Verifying Packages"
+        wget -P $LFS/sources https://www.linuxfromscratch.org/lfs/view/stable/md5sums
+
     elif [[ $option == "2" ]]; then
         echo 'nuh'
     elif [[ $option == "3" ]]; then 
@@ -28,3 +38,4 @@ do
     fi
         
 done
+

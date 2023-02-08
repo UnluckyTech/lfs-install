c7a7fde515d233ba170037b61fac995b9da0787d
UnluckyTech <stawse99@protonmail.com>
2023-02-07 23:34:34 -0500

    Chroot

--- temptools.sh
+++ temptools.sh
@@ -513,7 +513,205 @@ EOF
                     rm -rf gcc-12.2.0
                     echo "Installation Completed"
                 elif [[ $temp == "3" ]]; then
-                    echo "nuh"
+                    echo ''
+                    echo '*********************************'
+                    echo '********** Enter Chroot *********'
+                    echo '*********************************'
+                    echo '1. Changing Ownership'
+                    echo '2. Enter Chroot'
+                    echo '3. Create Directories'
+                    echo '4. Build Additional Temp Tools'
+                    echo '5. Clean and Backup Temp System'
+                    echo '6. Return to TempTools'
+                    read inpchr
+                    if [[ $inpchr == "1" ]]; then
+                        echo "Changing Ownership"
+                        chown -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
+                        case $(uname -m) in
+                        x86_64) chown -R root:root $LFS/lib64 ;;
+                        esac
+                        echo "Preparing Virtual Kernel File Systems"
+                        mkdir -pv $LFS/{dev,proc,sys,run}
+                        mount -v --bind /dev $LFS/dev
+                        mount -v --bind /dev/pts $LFS/dev/pts
+                        mount -vt proc proc $LFS/proc
+                        mount -vt sysfs sysfs $LFS/sys
+                        mount -vt tmpfs tmpfs $LFS/run
+                        if [ -h $LFS/dev/shm ]; then
+                            mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+                        fi
+                    elif [[ $inpchr == "2" ]]; then
+                        chroot "$LFS" /usr/bin/env -i   \
+                            HOME=/root                  \
+                            TERM="$TERM"                \
+                            PS1='(lfs chroot) \u:\w\$ ' \
+                            PATH=/usr/bin:/usr/sbin     \
+                            /bin/bash --login
+                    elif [[ $inpchr == "3" ]]; then
+                        echo "Creating Directories"
+                        mkdir -pv /{boot,home,mnt,opt,srv}
+                        mkdir -pv /etc/{opt,sysconfig}
+                        mkdir -pv /lib/firmware
+                        mkdir -pv /media/{floppy,cdrom}
+                        mkdir -pv /usr/{,local/}{include,src}
+                        mkdir -pv /usr/local/{bin,lib,sbin}
+                        mkdir -pv /usr/{,local/}share/{color,dict,doc,info,locale,man}
+                        mkdir -pv /usr/{,local/}share/{misc,terminfo,zoneinfo}
+                        mkdir -pv /usr/{,local/}share/man/man{1..8}
+                        mkdir -pv /var/{cache,local,log,mail,opt,spool}
+                        mkdir -pv /var/lib/{color,misc,locate}
+
+                        ln -sfv /run /var/run
+                        ln -sfv /run/lock /var/lock
+
+                        install -dv -m 0750 /root
+                        install -dv -m 1777 /tmp /var/tmp
+                        echo "Creating Essential Files and Symlinks"
+                        ln -sv /proc/self/mounts /etc/mtab
+                        cat > /etc/hosts << EOF
+                        127.0.0.1  localhost $(hostname)
+                        ::1        localhost
+EOF
+                        cat > /etc/passwd << "EOF"
+                        root:x:0:0:root:/root:/bin/bash
+                        bin:x:1:1:bin:/dev/null:/usr/bin/false
+                        daemon:x:6:6:Daemon User:/dev/null:/usr/bin/false
+                        messagebus:x:18:18:D-Bus Message Daemon User:/run/dbus:/usr/bin/false
+                        uuidd:x:80:80:UUID Generation Daemon User:/dev/null:/usr/bin/false
+                        nobody:x:65534:65534:Unprivileged User:/dev/null:/usr/bin/false
+EOF
+                        cat > /etc/group << "EOF"
+                        root:x:0:
+                        bin:x:1:daemon
+                        sys:x:2:
+                        kmem:x:3:
+                        tape:x:4:
+                        tty:x:5:
+                        daemon:x:6:
+                        floppy:x:7:
+                        disk:x:8:
+                        lp:x:9:
+                        dialout:x:10:
+                        audio:x:11:
+                        video:x:12:
+                        utmp:x:13:
+                        usb:x:14:
+                        cdrom:x:15:
+                        adm:x:16:
+                        messagebus:x:18:
+                        input:x:24:
+                        mail:x:34:
+                        kvm:x:61:
+                        uuidd:x:80:
+                        wheel:x:97:
+                        users:x:999:
+                        nogroup:x:65534:
+EOF
+                        echo "tester:x:101:101::/home/tester:/bin/bash" >> /etc/passwd
+                        echo "tester:x:101:" >> /etc/group
+                        install -o tester -d /home/tester
+                        exec /usr/bin/bash --login
+                        touch /var/log/{btmp,lastlog,faillog,wtmp}
+                        chgrp -v utmp /var/log/lastlog
+                        chmod -v 664  /var/log/lastlog
+                        chmod -v 600  /var/log/btmp
+                    elif [[ $inpchr == "4" ]]; then
+                        cd $LFS/sources
+                        echo "Installing Gettext (1/6)"
+                        sleep 1
+                        tar -xvf gettext-0.21.tar.xz
+                        cd gettext-0.21
+                        ./configure --disable-shared
+                        make
+                        cp -v gettext-tools/src/{msgfmt,msgmerge,xgettext} /usr/bin
+                        cd $LFS/sources
+                        rm -rf gettext-0.21
+                        echo "Installing Bison (2/6)"
+                        sleep 1
+                        tar -xvf bison-3.8.2.tar.xz
+                        cd bison-3.8.2
+                        ./configure --prefix=/usr \
+                                    --docdir=/usr/share/doc/bison-3.8.2
+                        make
+                        make install
+                        cd $LFS/sources
+                        rm -rf bison-3.8.2
+                        echo "Installing Perl (3/6)"
+                        sleep 1
+                        tar -xvf perl-5.36.0.tar.xz
+                        cd perl-5.36.0
+                        sh Configure -des                                        \
+                                     -Dprefix=/usr                               \
+                                     -Dvendorprefix=/usr                         \
+                                     -Dprivlib=/usr/lib/perl5/5.36/core_perl     \
+                                     -Darchlib=/usr/lib/perl5/5.36/core_perl     \
+                                     -Dsitelib=/usr/lib/perl5/5.36/site_perl     \
+                                     -Dsitearch=/usr/lib/perl5/5.36/site_perl    \
+                                     -Dvendorlib=/usr/lib/perl5/5.36/vendor_perl \
+                                     -Dvendorarch=/usr/lib/perl5/5.36/vendor_perl
+                        make
+                        make install
+                        cd $LFS/sources
+                        rm -rf perl-5.36.0
+                        echo "Installing Python (4/6)"
+                        sleep 1
+                        tar -xvf Python-3.10.6.tar.xz
+                        cd Python-3.10.6
+                        ./configure --prefix=/usr   \
+                                    --enable-shared \
+                                    --without-ensurepip
+                        make
+                        make install
+                        cd $LFS/sources
+                        rm -rf Python-3.10.6
+                        echo "Installing Texinfo (5/6)"
+                        sleep 1
+                        tar -xvf texinfo-6.8.tar.xz
+                        cd texinfo-6.8
+                        ./configure --prefix=/usr
+                        make
+                        make install
+                        cd $LFS/sources
+                        rm -rf texinfo-6.8
+                        echo "Installing Util Linux (6/6)"
+                        sleep 1
+                        tar -xvf util-linux-2.38.1.tar.xz
+                        cd util-linux-2.38.1
+                        ./configure ADJTIME_PATH=/var/lib/hwclock/adjtime    \
+                                    --libdir=/usr/lib    \
+                                    --docdir=/usr/share/doc/util-linux-2.38.1 \
+                                    --disable-chfn-chsh  \
+                                    --disable-login      \
+                                    --disable-nologin    \
+                                    --disable-su         \
+                                    --disable-setpriv    \
+                                    --disable-runuser    \
+                                    --disable-pylibmount \
+                                    --disable-static     \
+                                    --without-python     \
+                                    runstatedir=/run
+                        make
+                        make install
+                        cd $LFS/sources
+                        rm -rf util-linux-2.38.1
+                        echo "Installation Completed"
+                        echo "Cleaning Up..."
+                        rm -rf /usr/share/{info,man,doc}/*
+                        find /usr/{lib,libexec} -name \*.la -delete
+                        rm -rf /tools
+                    
+                    elif [[ $inpchr == "5" ]]; then
+                        echo "Backing Up..."
+                        umount $LFS/dev/pts
+                        umount $LFS/{sys,proc,run,dev}
+                        cd $LFS
+                        tar -cJpf $HOME/lfs-temp-tools-11.2.tar.xz .
+                        echo "Back Up Completed."
+                    elif [[ $inpchr == "6" ]]; then
+                        exit
+                    fi
+
+
                 elif [[ $temp == "4" ]]; then
                     echo "nuh"
                 elif [[ $temp == "5" ]]; then

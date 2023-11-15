3d23d28fc1fa49d6afc45df9bd5b480017155031
UnluckyTech <stawse99@protonmail.com>
2023-11-15 12:36:30 -0500

    Update temptools.sh

--- temptools.sh
+++ temptools.sh
@@ -114,6 +114,7 @@ EOF
                     read core
                     echo "export MAKEFLAGS='-j$core'" >> ~/.bashrc
                     source ~/.bash_profile
+                    return
                 elif [[ $finalprep == "3" ]]; then
                      . /home/$user/lfs-install/temptools.sh
                     return
